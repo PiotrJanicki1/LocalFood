89b2e75cf9e475e1f45b87563d89bafec2953ef9
PiotrJanicki1 <piotrjanicki222@gmail.com>
2024-08-03 21:47:52 +0200

    Add initial models for User, Category, Product, ProductImage, Address, Order, and OrderImage
    - Extend AbstractUser with custom User model
    - Create Category model
    - Create Product model
    - Create ProductImage model
    - Create Address model
    - Create Order model
    - Create OrderImage model

--- LocalFood_shop/LocalFood/settings.py
+++ LocalFood_shop/LocalFood/settings.py
@@ -1,3 +1,5 @@
+import os
+from google.oauth2 import service_account
 """
 Django settings for LocalFood project.
 
@@ -38,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'localfood_app',
+    'storages',
 ]
 
 MIDDLEWARE = [
@@ -126,3 +129,16 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# storage
+GS_CREDENTIALS = service_account.Credentials.from_service_account_file(
+    os.path.join(BASE_DIR, 'zippy-starlight-430706-s0-05bbab55e3a8.json')
+)
+DEFAULT_FILE_STORAGE = 'storages.backends.gcloud.GoogleCloudStorage'
+GS_BUCKET_NAME = 'local_food'
+
+
+# extend User model
+
+AUTH_USER_MODEL = 'localfood_app.User'
