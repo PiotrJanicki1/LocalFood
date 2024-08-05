4021b70e310e2c94ea0df000cd70dfe768f3cf5d
PiotrJanicki1 <piotrjanicki222@gmail.com>
2024-08-05 19:40:05 +0200

    Added new user registration handling with account type
    - Created a user registration form with an 'account_type' field allowing selection between 'business' and 'consumer'.
    - Implemented a new CreateUserView to handle registration, setting 'is_seller' or 'is_buyer' flags based on the selected 'account_type'.
    - Added a new URL configuration to handle user registration requests.
    
    These changes include a complete implementation of user registration functionality with account type consideration.

---
+++ localfood_app/validators.py
@@ -0,0 +1,8 @@
+from django.core.exceptions import ValidationError
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+def validate_username_unique(value):
+    if User.objects.filter(username=value):
+        raise ValidationError('Taka nazwa użytkownika jest już w użyciu')
