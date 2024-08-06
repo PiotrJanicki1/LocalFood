0a9c3aef5146a86f70f4e53ebb72af6a10be5cb9
PiotrJanicki1 <piotrjanicki222@gmail.com>
2024-08-06 21:35:43 +0200

    Enhanced project with new context processor and sales page view
    - Added a context processor in  to include additional context data across templates.
    - Implemented a new sales page view in  to handle sales-related page requests.
    - Updated various templates:
      -
      -
      -
      -
      to improve layout and styling.
    - Adjusted URL configurations in  to accommodate new view.
    - Modified  for context data handling improvements.

--- localfood_app/urls.py
+++ localfood_app/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 import localfood_app.views as views
 from .views import (
     HomePageView,
+    SalesPageView
 )
 
 
@@ -9,6 +10,6 @@ app_name = 'localfood_app'
 
 
 urlpatterns = [
-    path('', HomePageView.as_view(), name='home'),
-
+    path('home/', HomePageView.as_view(), name='home'),
+    path('sales/', SalesPageView.as_view(), name='sales'),
 ]
